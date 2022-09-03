a4a49710a17ec9941e6e794ad489742238da78c8
marvin9871 <112686554+marvin9871@users.noreply.github.com>
2022-09-04 01:29:06 +0700

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -10,22 +10,50 @@
 # You can set this variable whatever you want in shell session right before running this script by issuing:
 # export DAT_PATH='/usr/local/lib/v2ray'
 
-#!/usr/bin/env sh
+DAT_PATH=${DAT_PATH:-/etc/mosdns}
 
-GEOIP_URL="https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geoip.dat"
-NEW_GEOIP="./geoip.dat"
-GEOIP_PATH="/etc/mosdns/geoip.dat"
+DOWNLOAD_LINK_GEOIP="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
+DOWNLOAD_LINK_GEOSITE="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
+file_ip='geoip.dat'
+file_dlc='geosite.dat'
+dir_tmp="$(mktemp -d)"
 
-# Grab hosts file
-wget -O $NEW_GEOIP $GEOIP_URL
+download_files() {
+  if ! wget -q --no-cache -O "${dir_tmp}/${2}" "${1}"; then
+    echo 'error: Download failed! Please check your network or try again.'
+    exit 1
+  fi
+  if ! wget -q --no-cache -O "${dir_tmp}/${2}.sha256sum" "${1}.sha256sum"; then
+    echo 'error: Download failed! Please check your network or try again.'
+    exit 1
+  fi
+}
 
-cp -v $NEW_GEOIP $GEOIP_PATH
+check_sum() {
+  (
+    cd "${dir_tmp}" || exit
+    for i in "${dir_tmp}"/*.sha256sum; do
+      if ! sha256sum -c "${i}"; then
+        echo 'error: Check failed! Please check your network or try again.'
+        exit 1
+      fi
+    done
+  )
+}
 
-GEOSITE_URL="https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geosite.dat"
-NEW_GEOSITE="./geosite.dat"
-GEOSITE_PATH="/etc/mosdns/geosite.dat"
+install_file() {
+  mkdir -p ${DAT_PATH} 2>/dev/null
+  cp -af "${dir_tmp}"/${file_dlc} "${DAT_PATH}"/${file_dlc}
+  cp -af "${dir_tmp}"/${file_ip} "${DAT_PATH}"/${file_ip}
+  rm -r "${dir_tmp}"
+}
 
-# Grab hosts file
-wget -O $NEW_GEOSITE $GEOSITE_URL
+main() {
+  echo "Updating geoip.dat and geosite.dat"
+  download_files $DOWNLOAD_LINK_GEOIP $file_ip
+  download_files $DOWNLOAD_LINK_GEOSITE $file_dlc
+  check_sum
+  install_file
+}
 
-cp -v $NEW_GEOSITE $GEOSITE_PATH
+main "$@"
